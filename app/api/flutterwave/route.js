026f6ddb96b9fd1c1a5b1c2de09f52365ef45c80
Mishael Joseph <mishaeljoe55@gmail.com>
2023-11-24 13:20:55 +0100

    Update route.js

--- app/api/flutterwave/route.js
+++ app/api/flutterwave/route.js
@@ -29,7 +29,7 @@ export async function POST(request) {
           postal_code: postal_code ,
           city: city,
           state: state,
-          itemsInCart: {...itemsInCart},
+          itemsInCart: itemsInCart,
         },
         customer: {
           email: email,
