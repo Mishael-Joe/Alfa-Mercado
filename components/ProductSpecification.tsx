e4b1867869149be07742cba8a92e210dd4ec09d1
mishael joseph <mishaeljoe55@gmail.com>
2023-12-21 09:59:06 +0100

    modified:   components/ProductSpecification.tsx

--- components/ProductSpecification.tsx
+++ components/ProductSpecification.tsx
@@ -1,7 +1,7 @@
 
 export function ProductSpecification({ product }: any) {
     // console.log('product.specifications.length',product)
-    if (product.specifications.length === 0) return;
+    if (product.specifications === null) return;
 
     return (
         <div className="mb-10 rounded-lg border-2 border-gray-200 bg-gray-100 px-4 py-6 shadow dark:border-gray-900 dark:bg-black sm:p-6 lg:p-8">
